a21f68ebfd2558f4b83e074ab6da2334cced3a6f
Amira Ashraf <amiraashraf0210@gmail.com>
2024-07-15 11:11:45 +0300

    Add project files.

---
+++ 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_project
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] name;
+            int[] age;
+
+            Console.WriteLine("enter the length of the arrays");
+            int size = int.Parse(Console.ReadLine());
+            name = new string[size];
+            age = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("enter the values");
+                name[i] = Console.ReadLine();
+                age[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("which member do you want to know? ");
+            int j = int.Parse(Console.ReadLine());
+            Console.WriteLine(name[j]);
+            Console.WriteLine(age[j]);
+        }
+
+
+
+
+    }
+}
